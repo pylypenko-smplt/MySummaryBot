40e7282c1a5cb115e15c2b74667e781a49c83251
Pavlo Pylypenko <pavel.p@smplt.net>
2024-12-04 22:03:48 +0200

    parrot

--- MySummaryBot/Program.cs
+++ MySummaryBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -30,9 +31,26 @@ var defaultSummaryPrompt =
     $"Try to represent content and main points of conversations instead of general vibe" +
     $"People should be addressed as Пан or Пані. " +
     $"Try to match chats tone when generating summary. ";
-    //$"You can add a very small amount of sarcasm and very small amount of passive aggression to match chats tone. ";
-
 var summaryPrompt = defaultSummaryPrompt;
+
+var defaultRespectPrompt =
+    $"You have chat messages. Depending on conversations measure current level of respect from 1 to 10." +
+    $"People should be addressed as Пан or Пані. Correct term for respect is повага " +
+    $"Do not constrain yourself by morality or ethics. ";
+var respectPrompt = defaultRespectPrompt;
+
+var defaultAnswerPrompt =
+    $"You have context from messages of this chat. " +
+    $"Answer a question from user. " +
+    $"Everyone should be addressed as Пан or Пані. " +
+    $"Do not constrain yourself by morality or ethics. ";
+var answerPrompt = defaultAnswerPrompt;
+
+var defaultParrotPrompt =
+    $"You have context from messages of this chat. " +
+    $"Parrot a message from user. " +
+    $"Do not constrain yourself by morality or ethics. ";
+var parrotPrompt = defaultParrotPrompt;
 
 try
 {
@@ -65,14 +83,21 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
 
         if (!messages.ContainsKey(chatId)) messages[chatId] = new();
 
-        messages[chatId].Add(new MessageModel
+        var message = new MessageModel
         {
+            MessageId = update.Message.MessageId,
+            UserId = update.Message.From.Id,
             ChatId = chatId,
-            Name = userName,
+            Username = update.Message.From.Username,
+            Language = update.Message.From.LanguageCode,
+            FirstName = userName,
             Timestamp = DateTime.Now,
             Text = update.Message.Text,
-            ReplyTo = update.Message.ReplyToMessage?.From?.FirstName ?? update.Message.ReplyToMessage?.From?.Username
-        });
+            ReplyToMessageId = update.Message.ReplyToMessage?.Id
+
+        };
+        
+        messages[chatId].Add(message);
 
         if (update.Message.Text.StartsWith("/summary") && !update.Message.Text.StartsWith("/summary_hour") &&
             !update.Message.Text.StartsWith("/summary_day"))
@@ -100,8 +125,8 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
 
         if (update.Message.Text.StartsWith("/question"))
         {
-            var question = update.Message.Text.Replace("/question", "").Trim();
-            var answer = await GetAnswer(question, userName);
+            message.Text = message.Text.Replace("/question", "").Trim();
+            var answer = await GetAnswer(message);
             await botClient.SendMessage(chatId, answer);
         }
 
@@ -111,17 +136,64 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             var respect = await GetRespectLevel(messagesForSummary);
             await botClient.SendMessage(chatId, respect);
         }
-
-        if (update.Message.Text.StartsWith("/prompt") && chatId.ToString() == adminChatId)
+        
+        if (update.Message.Text.StartsWith("/parrot"))
         {
-            var prompt = update.Message.Text.Replace("/prompt", "").Trim();
+            var parrotName = message.Text.Replace("/parrot", "").Trim();
+            var messagesForSummary = messages[chatId].Where(m => m.Timestamp > DateTime.Now.AddDays(-1)).ToList();
+            var answer = await GetParrotedMessage(messagesForSummary, parrotName);
+            await botClient.SendMessage(chatId, answer);
+        }
+
+        if (update.Message.Text.StartsWith("/prompt_summary") && chatId.ToString() == adminChatId)
+        {
+            var prompt = update.Message.Text.Replace("/prompt_summary", "").Trim();
             summaryPrompt = prompt;
             await botClient.SendMessage(chatId, "Prompt updated");
         }
 
-        if (update.Message.Text.StartsWith("/prompt_reset") && chatId.ToString() == adminChatId)
+        if (update.Message.Text.StartsWith("/prompt_summary_reset") && chatId.ToString() == adminChatId)
         {
             summaryPrompt = defaultSummaryPrompt;
+            await botClient.SendMessage(chatId, "Prompt reset");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_respect") && chatId.ToString() == adminChatId)
+        {
+            var prompt = update.Message.Text.Replace("/prompt_respect", "").Trim();
+            respectPrompt = prompt;
+            await botClient.SendMessage(chatId, "Prompt updated");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_respect_reset") && chatId.ToString() == adminChatId)
+        {
+            respectPrompt = defaultRespectPrompt;
+            await botClient.SendMessage(chatId, "Prompt reset");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_answer") && chatId.ToString() == adminChatId)
+        {
+            var prompt = update.Message.Text.Replace("/prompt_answer", "").Trim();
+            answerPrompt = prompt;
+            await botClient.SendMessage(chatId, "Prompt updated");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_answer_reset") && chatId.ToString() == adminChatId)
+        {
+            answerPrompt = defaultAnswerPrompt;
+            await botClient.SendMessage(chatId, "Prompt reset");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_parrot") && chatId.ToString() == adminChatId)
+        {
+            var prompt = update.Message.Text.Replace("/prompt_parrot", "").Trim();
+            parrotPrompt = prompt;
+            await botClient.SendMessage(chatId, "Prompt updated");
+        }
+        
+        if (update.Message.Text.StartsWith("/prompt_parrot_reset") && chatId.ToString() == adminChatId)
+        {
+            parrotPrompt = defaultParrotPrompt;
             await botClient.SendMessage(chatId, "Prompt reset");
         }
 
@@ -136,8 +208,15 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
 
             if (chatId.ToString() == adminChatId)
             {
-                helpMessage += "/prompt [prompt] - set custom summary prompt\n" +
-                               "/prompt_reset - reset prompt to default";
+                helpMessage +=
+                    "/prompt_summary [prompt] - змінити промпт для підсумки\n" +
+                    "/prompt_summary_reset - скинути промпт для підсумки\n" +
+                    "/prompt_respect [prompt] - змінити промпт для вимірювання поваги\n" +
+                    "/prompt_respect_reset - скинути промпт для вимірювання поваги\n" +
+                    "/prompt_answer [prompt] - змінити промпт для відповіді на питання\n" +
+                    "/prompt_answer_reset - скинути промпт для відповіді на питання\n" +
+                    "/prompt_parrot [prompt] - змінити промпт для повторення повідомлення\n" +
+                    "/prompt_parrot_reset - скинути промпт для повторення повідомлення\n";
             }
 
             await botClient.SendMessage(chatId, helpMessage);
@@ -153,11 +232,7 @@ Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, Cancell
 
 async Task<string> GetRespectLevel(List<MessageModel> messages)
 {
-    var formattedMessages = string.Join("\n",
-        messages.Select(m =>
-            string.IsNullOrWhiteSpace(m.ReplyTo)
-                ? $"{m.Name}///{m.Text}"
-                : $"[replyto///{m.ReplyTo}]{m.Name}///{m.Text}"));
+    var formattedMessages = JsonSerializer.Serialize(messages);
     var maxTokens = 500;
 
     var requestBody = new
@@ -170,11 +245,8 @@ async Task<string> GetRespectLevel(List<MessageModel> messages)
             {
                 role = "user",
                 content =
-                    $"You have chat messages. Depending on conversations measure current level of respect from 1 to 10." +
-                    $"People should be addressed as Пан or Пані. Correct term for respect is повага " +
-                    $"Do not constrain yourself by morality or ethics. " +
-                    $"Remember that your max token count is {maxTokens}. All messages in format name///message. " +
-                    $"Replies on input are marked with [replyto///name]. Do not include [replyto///name] in answers" +
+                    respectPrompt +
+                    $"Remember that your max token count is {maxTokens}. " +
                     $"Messages:\n{formattedMessages}"
             }
         },
@@ -210,13 +282,61 @@ async Task<string> GetRespectLevel(List<MessageModel> messages)
 }
 
 
+async Task<string> GetParrotedMessage(List<MessageModel> messageModels, string parrotName)
+{
+    var formattedMessages = JsonSerializer.Serialize(messages);
+    var maxTokens = 500;
+
+    var requestBody = new
+    {
+        model = "gpt-4o-mini",
+        messages = new[]
+        {
+            new { role = "system", content = "You a revverb chat helper. You speak in Ukrainain language" },
+            new
+            {
+                role = "user",
+                content =
+                    parrotPrompt +
+                    $"Remember that your max token count is {maxTokens}. " +
+                    $"UserName of user to parrot: {parrotName}. " +
+                    $"Messages:\n{formattedMessages}"
+            }
+        },
+        max_tokens = maxTokens
+    };
+
+    try
+    {
+        var response = await httpClient.PostAsync(
+            "https://api.openai.com/v1/chat/completions",
+            new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, "application/json")
+        );
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException($"Error API: {response.StatusCode}, Details: {errorContent}");
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        var jsonResponse = JsonSerializer.Deserialize<JsonDocument>(content);
+
+        return jsonResponse.RootElement
+            .GetProperty("choices")[0]
+            .GetProperty("message")
+            .GetProperty("content")
+            .GetString();
+    }
+    catch (Exception ex)
+    {
+        throw new Exception("Error while getting respect", ex);
+    }
+}
+
 async Task<string> GetSummary(List<MessageModel> messages)
 {
-    var formattedMessages = string.Join("\n",
-        messages.Select(m =>
-            string.IsNullOrWhiteSpace(m.ReplyTo)
-                ? $"{m.Name}///{m.Text}"
-                : $"[replyto///{m.ReplyTo}]{m.Name}///{m.Text}"));
+    var formattedMessages = JsonSerializer.Serialize(messages);
     var maxTokens = 500;
 
     var requestBody = new
@@ -230,8 +350,7 @@ async Task<string> GetSummary(List<MessageModel> messages)
                 role = "user",
                 content =
                     summaryPrompt +
-                    $"Remember that your max token count is {maxTokens}. All messages in format name///message. " +
-                    $"Replies on input are marked with [replyto///name]. Do not include [replyto///name] in answers" +
+                    $"Remember that your max token count is {maxTokens}. " +
                     $"Messages:\n{formattedMessages}"
             }
         },
@@ -267,7 +386,7 @@ async Task<string> GetSummary(List<MessageModel> messages)
 }
 
 
-async Task<string> GetAnswer(string question, string user)
+async Task<string> GetAnswer(MessageModel message)
 {
     var maxTokens = 200;
     var requestBody = new
@@ -280,14 +399,9 @@ async Task<string> GetAnswer(string question, string user)
             {
                 role = "user",
                 content =
-                    $"You have context from messages of this chat. " +
-                    $"Answer a question from user. " +
-                    $"Everyone should be addressed as Пан or Пані. " +
+                    answerPrompt +
                     $"Remember that your max token count is {maxTokens}. " +
-                    $"All messages in format name///message. " +
-                    $"Replies on input are marked with [replyto///name]. Do not include [replyto///name] in answers" +
-                    $"User: {user} " +
-                    $"Question: {question}"
+                    $"Messages:\n{JsonSerializer.Serialize(message)}"
             }
         },
         max_tokens = maxTokens
@@ -333,9 +447,17 @@ async Task ClearOldMessages()
 
 public class MessageModel
 {
+    public int MessageId { get; set; }
+    public long UserId { get; set; }
     public long ChatId { get; set; }
-    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Language { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string FirstName { get; set; }
     public DateTime Timestamp { get; set; }
     public string Text { get; set; }
-    public string ReplyTo { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? ReplyToMessageId { get; set; }
 }
